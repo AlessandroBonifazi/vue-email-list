ef0434502eef50a13214c0dc625e84ee8d5ba640
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-04-04 16:59:02 +0200

    adding axios and DOM

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 // Debug
 console.log('JS OK!');
 
-axios.get()
-
 // Config
 const app = new Vue({
     el: '#root',
     data: {
-
+        emails: [],
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                const email = response.data;
+                this.emails.push(email.response)
+            });
+        }
     },
     methods: {
 
